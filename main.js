fbcd2b48c330a14f288367d5f11bf91d84d49f4d
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-11 18:41:54 +0200

    finish section gallery html,styles,modal,js function

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ toggle.addEventListener("click", () => {
   nav.classList.toggle("show-menu");
   toggle.classList.toggle("toggleX");
 });
+
+// Get the modal
+const modal = document.querySelector(".modal");
+
+const images = document.querySelectorAll(".gallery-img");
+const modalImg = document.querySelector(".modal-content");
+const closeX = document.querySelector(".close");
+
+// Show Modal
+const showModal = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+// Close X Modal
+const closeModal = function () {
+  modal.style.display = "none";
+};
+
+// Event Handler
+for (let i = 0; i < images.length; i++)
+  images[i].addEventListener("click", showModal);
+
+closeX.addEventListener("click", closeModal);
+
+// Close Modal-key esc
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
